f41e3835cf4cd30f9de283d767b4f6803e035fa0
SWspice <32869306+SWspice@users.noreply.github.com>
2017-10-17 15:09:18 +0200

    Add files via upload

---
+++ background.js
@@ -0,0 +1,21 @@
+/* jslint devel: true, browser: true, indent: 4, evil: true */
+/* global chrome, $, gapi, B64 */
+
+(function() {
+    "use strict";
+
+    /*
+    function dummy(request, port){
+        port.postMessage(resp)
+    }*/
+
+    // Add listeners for communication from client
+    chrome.runtime.onConnect.addListener(function(port) {
+        /*
+        if (port.name === "dummy") {
+            port.onMessage.addListener(dummy)
+        }
+        */
+    });
+
+}());
